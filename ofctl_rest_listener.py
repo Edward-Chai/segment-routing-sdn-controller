71c68c77c0c3dc9c41065cf78aeeabdbf84d2d96
zehuazhai <edward.zhai@yahoo.com>
2022-11-24 04:32:36 +0900

    Test Msg 20221124 4:32

--- ofctl_rest_listener.py
+++ ofctl_rest_listener.py
@@ -1,18 +1,18 @@
 #!/usr/bin/python2.7
 
-#Copyright Binh Nguyen University of Utah (binh@cs.utah.edu)
+# Copyright Binh Nguyen University of Utah (binh@cs.utah.edu)
 #
-#Licensed under the Apache License, Version 2.0 (the "License");
-#you may not use this file except in compliance with the License.
-#You may obtain a copy of the License at
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
 #
 #    http://www.apache.org/licenses/LICENSE-2.0
 #
-#Unless required by applicable law or agreed to in writing, software
-#distributed under the License is distributed on an "AS IS" BASIS,
-#WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-#See the License for the specific language governing permissions and
-#limitations under the License.
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
 
 import logging
 
@@ -48,6 +48,7 @@ supported_ofctl = {
     ofproto_v1_3.OFP_VERSION: ofctl_v1_3,
 }
 
+
 # REST API LISTENER FOR SR CONTROLLER
 
 class SR_rest_api(app_manager.RyuApp):
@@ -55,6 +56,7 @@ class SR_rest_api(app_manager.RyuApp):
         'dpset': dpset.DPSet,
         'wsgi': WSGIApplication
     }
+
     def __init__(self, *args, **kwargs):
         super(SR_rest_api, self).__init__(*args, **kwargs)
         LOG.debug("Init SR_rest_api")
@@ -67,11 +69,10 @@ class SR_rest_api(app_manager.RyuApp):
         mapper = wsgi.mapper
 
         wsgi.registory['North_api'] = self.data
-        
 
         flow_mgmt = "flow_mgmt"
-	ospf_monitor = "ospf_monitor"
-	ospf_monitor_path = "/%s" % ospf_monitor
+        ospf_monitor = "ospf_monitor"
+        ospf_monitor_path = "/%s" % ospf_monitor
         flow_mgmt_path = '/%s' % flow_mgmt
         uri = flow_mgmt_path + '/delete'
         mapper.connect(flow_mgmt, uri,
@@ -83,41 +84,35 @@ class SR_rest_api(app_manager.RyuApp):
                        controller=North_api, action='delete_all_flows',
                        conditions=dict(method=['POST']))
 
-
-	#Usage: curl --data 'dpid=17779080870&match=ipv6_dst=2001::204:23ff:feb7:1e40,eth_type=0x86DD&actions=ipv6_dst=2001::208:204:23ff:feb7:1e40,ipv6_dst=2001::208:204:23ff:feb7:1e41,ipv6_dst=2001::208:204:23ff:feb7:1e42,output=1' http://0.0.0.0:8080/flow_mgmt/insert
+        # Usage: curl --data 'dpid=17779080870&match=ipv6_dst=2001::204:23ff:feb7:1e40,eth_type=0x86DD&actions=ipv6_dst=2001::208:204:23ff:feb7:1e40,ipv6_dst=2001::208:204:23ff:feb7:1e41,ipv6_dst=2001::208:204:23ff:feb7:1e42,output=1' http://0.0.0.0:8080/flow_mgmt/insert
         uri = flow_mgmt_path + '/insert'
         mapper.connect(flow_mgmt, uri,
                        controller=North_api, action='insert_single_flow',
                        conditions=dict(method=['POST']))
-     	uri = flow_mgmt_path + '/insert'
+        uri = flow_mgmt_path + '/insert'
         mapper.connect(flow_mgmt, uri,
                        controller=North_api, action='handle_http_options',
                        conditions=dict(method=['OPTIONS']))
-
 
         uri = ospf_monitor_path + '/lsa_put'
         mapper.connect(ospf_monitor, uri,
                        controller=Te_controller, action='receive_ospf_lsa',
                        conditions=dict(method=['POST']))
 
-	uri = ospf_monitor_path + '/get_topology'
+        uri = ospf_monitor_path + '/get_topology'
         mapper.connect(ospf_monitor, uri,
                        controller=Te_controller, action='handle_get_topology_OPTIONS',
                        conditions=dict(method=['OPTIONS']))
 
-	uri = ospf_monitor_path + '/get_topology'
+        uri = ospf_monitor_path + '/get_topology'
         mapper.connect(ospf_monitor, uri,
                        controller=Te_controller, action='get_topology',
                        conditions=dict(method=['GET']))
 
-	uri = ospf_monitor_path + '/get_topology_netjson'
+        uri = ospf_monitor_path + '/get_topology_netjson'
         mapper.connect(ospf_monitor, uri,
                        controller=Te_controller, action='get_topology_netjson',
                        conditions=dict(method=['GET']))
-
-
-
-
 
     @set_ev_cls([ofp_event.EventOFPStatsReply,
                  ofp_event.EventOFPDescStatsReply,
@@ -174,7 +169,3 @@ class SR_rest_api(app_manager.RyuApp):
 
         del self.waiters[dp.id][msg.xid]
         lock.set()
-
-
-
-
