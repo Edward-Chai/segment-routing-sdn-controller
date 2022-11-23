87027f96ee5f102f27b234640f54a33b70430cea
zehuazhai <edward.zhai@yahoo.com>
2022-11-24 04:35:30 +0900

    Test Msg 20221124 4:35

--- northbound_actions.py
+++ northbound_actions.py
@@ -13,57 +13,57 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-#!/usr/bin/python
+# !/usr/bin/python
 
 import logging
+
 LOG = logging.getLogger('ryu.app.Actions')
 LOG.setLevel(logging.INFO)
 
+
 class Actions(object):
+    actions_fields = {
+        # all supported actions fields. eg, curl -d "actions="ipv6_dst=::01,mod_dl_src="AA:BB:CC:DD:EE:FF""
+        "ipv6_dst": [],
+        "mod_dl_dst": None,
+        "output": None
+    }
 
-  actions_fields = { #all supported actions fields. eg, curl -d "actions="ipv6_dst=::01,mod_dl_src="AA:BB:CC:DD:EE:FF""
-  "ipv6_dst": [],
-  "mod_dl_dst": None,
-  "output": None
-  }
-  
-  def get_actions_fields(self):
-    return actions_fields
+    def get_actions_fields(self):
+        return actions_fields
 
-  def parse_actions_fields(self,str):
-    LOG.debug("parse_actions_fields, str=%s" % str)
+    def parse_actions_fields(self, str):
+        LOG.debug("parse_actions_fields, str=%s" % str)
 
-    tokens = str.split(',')
-    for t in tokens:
-      try:
-      	key = t.split("=")[0]
-      	value = t.split("=")[1]
-      except:
-	LOG.error("Invalid actions: %s" % t)
-	return None
+        tokens = str.split(',')
+        for t in tokens:
+            try:
+                key = t.split("=")[0]
+                value = t.split("=")[1]
+            except:
+                LOG.error("Invalid actions: %s" % t)
+                return None
 
-      if key in self.actions_fields:
-        if key == "ipv6_dst":
-          self.actions_fields[key].append(value)
-        else:
-          self.actions_fields[key] = value
-      else:
-        LOG.warn("Key isn't supported: %s" % key)
-	
-    return self.actions_fields
+            if key in self.actions_fields:
+                if key == "ipv6_dst":
+                    self.actions_fields[key].append(value)
+                else:
+                    self.actions_fields[key] = value
+            else:
+                LOG.warn("Key isn't supported: %s" % key)
 
-  def __init__(self, *args, **kwagrs):
-    super(Actions, self).__init__(*args, **kwagrs)
-    self.actions_fields['ipv6_dst'] = []
-    for key in kwagrs:
-      if key == "ipv6_dst":
-        self.actions_fields[key].append(kwagrs[key])
-      else:
-        self.actions_fields[key] = kwagrs[key]
-    
+        return self.actions_fields
 
-  def print_me(self):
-    LOG.info("Actions_fields -> value")
-    for key in self.actions_fields:
-      LOG.info("%s -> %s" % (key, self.actions_fields[key]))
-    
+    def __init__(self, *args, **kwagrs):
+        super(Actions, self).__init__(*args, **kwagrs)
+        self.actions_fields['ipv6_dst'] = []
+        for key in kwagrs:
+            if key == "ipv6_dst":
+                self.actions_fields[key].append(kwagrs[key])
+            else:
+                self.actions_fields[key] = kwagrs[key]
+
+    def print_me(self):
+        LOG.info("Actions_fields -> value")
+        for key in self.actions_fields:
+            LOG.info("%s -> %s" % (key, self.actions_fields[key]))
