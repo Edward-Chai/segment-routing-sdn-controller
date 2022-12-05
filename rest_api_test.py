e7175b66166571026392532b8bf14ff299781e8e
zehuazhai <edward.zhai@yahoo.com>
2022-12-05 21:31:09 +0900

    Test Msg 20221205 21:31

--- rest_api_test.py
+++ rest_api_test.py
@@ -50,7 +50,7 @@ class SR_API_Controller(ControllerBase):
         req_body = req.body
         LOG.info(req_body)
         req_body_dec = req_body.decode('utf-8')
-        match_fields = SRv6_field_match.parse_match_fields(req_body)
+        match_fields = SRv6_field_match.parse_match_fields(req_body_dec)
         LOG.info("Match_fields:" + match_fields)
         # ssh = paramiko.SSHClient()
         # ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
