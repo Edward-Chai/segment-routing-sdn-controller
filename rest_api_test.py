a627f2a8bba0d1dad9f82baf6b128c0c2ecf3d68
zehuazhai <edward.zhai@yahoo.com>
2022-12-05 22:45:47 +0900

    Test Msg 20221205 22:45

--- rest_api_test.py
+++ rest_api_test.py
@@ -48,7 +48,7 @@ class SR_API_Controller(ControllerBase):
 
     def insert_single_flow(self, req, **kwargs):
         req_body = req.body
-        LOG.info(req_body)
+        LOG.debug(req_body)
         # req_body_dec = req_body.decode('utf-8')
         SRv6_match = SRv6_field_match()
         match_fields = SRv6_match.parse_match_fields(req_body)
