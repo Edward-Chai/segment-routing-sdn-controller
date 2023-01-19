f12a7466972ce297db7675c463e7086536a28c44
zehuazhai <edward.zhai@yahoo.com>
2023-01-19 09:43:17 +0900

    Test Msg 20220119 0943

--- funcMgr.py
+++ funcMgr.py
@@ -117,8 +117,8 @@ class FUNC_MGR_Controller(ControllerBase):
         infoConversion = info_conversion()
         reqHandler = reqHandling()
         reqIntraRegionPathComput = infoConversion.req_of_intra_region_path_comput(jsonMsg)
-        intra_mano_url = IntraMANOURL + ''
-        r = reqHandler.sendPost(InterRegionFuncMgrURL, reqIntraRegionPathComput)
+        intra_mano_url = IntraMANOURL + 'req/intraRegionPathComput'
+        r = reqHandler.sendPost(intra_mano_url, reqIntraRegionPathComput)
         print("jsonMsg:", jsonMsg, "\n")
 
 
